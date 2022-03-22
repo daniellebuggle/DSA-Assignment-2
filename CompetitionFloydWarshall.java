5519284ce2dfe695f0317245422ef9f6da49d8c0
daniellebuggle <99493192+daniellebuggle@users.noreply.github.com>
2022-03-22 16:46:33 +0000

    Finished Constructor for FloydWarshall

--- CompetitionFloydWarshall.java
+++ CompetitionFloydWarshall.java
@@ -15,15 +15,54 @@
  * This class implements the competition using Floyd-Warshall algorithm
  */
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class CompetitionFloydWarshall {
+    private final int sA;
+    private final int sB;
+    private final int sC;
+
+    private double[][] graph;
 
     /**
      * @param filename: A filename containing the details of the city road network
      * @param sA, sB, sC: speeds for 3 contestants
      */
     CompetitionFloydWarshall (String filename, int sA, int sB, int sC){
+        this.sA = sA; // walking speed of contestant A
+        this.sB = sB; // walking speed of contestant B
+        this.sC = sC; // walking speed of contestant C
 
-        //TODO
+        try {
+            File myObj = new File(filename);
+            Scanner myReader = new Scanner(myObj);
+            int count = 0;
+            int numberOfVertices;
+            int numberOfEdges = 0;
+            while (myReader.hasNextLine()) {
+                if(count == 0){
+                    numberOfVertices = myReader.nextInt();
+                    graph = new double[numberOfVertices - 1][numberOfVertices - 1];
+                    count++;
+                }else if(count == 1){
+                    numberOfEdges = myReader.nextInt();
+                    count++;
+                }else{
+                    for(int i = 0; i < numberOfEdges; i++){
+                        int v1 = myReader.nextInt();
+                        int v2 = myReader.nextInt();
+                        double weight = myReader.nextDouble();
+                        graph[v1][v2] = weight;
+                    }
+                }
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 
 
